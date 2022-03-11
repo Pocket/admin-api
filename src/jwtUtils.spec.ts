e5be72bae6d490fb37d242c0d44afcec0b24e863
Kelvin Yeboah <kyeboah@getpocket.com>
2022-03-11 14:51:41 -0800

    feat(auth): fetch jwks from cognito and pocket

--- src/jwtUtils.spec.ts
+++ src/jwtUtils.spec.ts
@@ -1,4 +1,7 @@
 import * as jwtUtils from './jwtUtils';
+import nock from 'nock';
+import config from './config';
+import { getSigningKeysFromServer } from './jwtUtils';
 
 describe('jwtUtils', () => {
   describe('buildAdminAPIUserFromPayload', () => {
@@ -36,6 +39,60 @@ describe('jwtUtils', () => {
       expect(() => {
         jwtUtils.buildAdminAPIUserFromPayload(badPayload);
       }).toThrow('JWT payload missing identity information');
+    });
+  });
+
+  describe('getSigningKeysFromServer', () => {
+    const cognitoJwks = {
+      keys: [
+        {
+          alg: 'RS256',
+          e: 'AQAB',
+          kid: 'kze4M0CiXoDO7Qkpig1oH0F6OInzZg6ugk0PyojOlzc=',
+          kty: 'RSA',
+          n: 'uXk44uWZ2tx',
+          use: 'sig',
+        },
+        {
+          alg: 'RS256',
+          e: 'AQAB',
+          kid: '4w35mrh4EBECpjJnyIjdQ60yjh3xeI1m0VF1H/z0T/c=',
+          kty: 'RSA',
+          n: '7LjXOvCWl6Y',
+          use: 'sig',
+        },
+      ],
+    };
+    const pocketJwks = {
+      keys: [
+        {
+          kty: 'RSA',
+          e: 'AQAB',
+          use: 'sig',
+          kid: 'CURMIG',
+          alg: 'RS256',
+          n: 'sI5Sttlr',
+        },
+      ],
+    };
+    it('should get keys from cognito and pocket', async () => {
+      const cognitoMock = nock('https://' + config.auth.cognito.jwtIssuer)
+        .persist()
+        .get('/.well-known/jwks.json')
+        .reply(200, cognitoJwks);
+      nock('https://' + config.auth.pocket.jwtIssuer)
+        .get('/.well-known/jwk')
+        .reply(200, pocketJwks);
+
+      const keys = await getSigningKeysFromServer();
+
+      expect(Object.keys(keys)).toEqual([
+        'kze4M0CiXoDO7Qkpig1oH0F6OInzZg6ugk0PyojOlzc=',
+        '4w35mrh4EBECpjJnyIjdQ60yjh3xeI1m0VF1H/z0T/c=',
+        'CURMIG',
+      ]);
+
+      cognitoMock.persist(false);
     });
   });
 });
